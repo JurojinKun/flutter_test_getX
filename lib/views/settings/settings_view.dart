4477b902ab613238cda40377311e8181da010e8b
0ruj <ccommunay@gmail.com>
2024-09-09 17:36:30 +0200

    - wip navigation bar logic

---
+++ lib/views/settings/settings_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingsView extends StatefulWidget {
+  const SettingsView({super.key});
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Settings"),
+      ),
+    );
+  }
+}

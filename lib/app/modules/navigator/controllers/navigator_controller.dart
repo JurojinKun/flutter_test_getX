7409a2a5067e7275b12603ead2b2726fa9153e58
0ruj <ccommunay@gmail.com>
2024-09-21 21:56:44 +0200

    Navigation part
    - add logic navigation controller to initialize each navigator only when you go on it and not all navigators at the same time
    - implement new logic pop until first page navigator pile bottom nav bar

--- lib/app/modules/navigator/controllers/navigator_controller.dart
+++ lib/app/modules/navigator/controllers/navigator_controller.dart
@@ -1,33 +1,63 @@
-import 'package:flutter_test_getx/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test_getx/app/modules/home/wrappers/home_wrapper.dart';
+import 'package:flutter_test_getx/app/modules/searching/wrappers/searching_wrapper.dart';
+import 'package:flutter_test_getx/app/modules/settings/wrappers/settings_wrapper.dart';
 import 'package:get/get.dart';
 
 class NavigatorController extends GetxController {
   final currentIndex = 0.obs;
+  
   final homeNavigatorKey = Get.nestedKey(1);
   final searchingNavigatorKey = Get.nestedKey(2);
   final settingsNavigatorKey = Get.nestedKey(3);
 
-  void changeIndex(int index) {
+  Rx<Widget?> homeWrapper = Rx<Widget?>(null);
+  Rx<Widget?> searchWrapper = Rx<Widget?>(null);
+  Rx<Widget?> settingsWrapper = Rx<Widget?>(null);
+
+  @override
+  void onInit() {
+    _initPageBottomBar(0);
+    super.onInit();
+  }
+
+  void _initPageBottomBar(int index) {
+    if (index == 0 && homeWrapper.value == null) {
+      homeWrapper.value = const HomeWrapper();
+    } else if (index == 1 && searchWrapper.value == null) {
+      searchWrapper.value = const SearchingWrapper();
+    } else if (index == 2 && settingsWrapper.value == null) {
+      settingsWrapper.value = const SettingsWrapper();
+    }
+  }
+
+  void changeIndex(int index) async {
     if (currentIndex.value == index) {
       switch (index) {
-      case 0:
-        if (homeNavigatorKey!.currentState != null && homeNavigatorKey!.currentState!.canPop()) {
-          Get.offAllNamed(Routes.home, id: 1);
-        }
-        break;
-      case 1:
-        if (searchingNavigatorKey!.currentState != null && searchingNavigatorKey!.currentState!.canPop()) {
-          Get.offAllNamed(Routes.searching, id: 2);
-        }
-        break;
-      case 2:
-        if (settingsNavigatorKey!.currentState != null && settingsNavigatorKey!.currentState!.canPop()) {
-          Get.offAllNamed(Routes.settings, id: 3);
-        }
-        break;
-    }
+        case 0:
+          if (homeNavigatorKey!.currentState != null &&
+              homeNavigatorKey!.currentState!.canPop()) {
+            homeNavigatorKey!.currentState!.popUntil((route) => route.isFirst);
+          }
+          break;
+        case 1:
+          if (searchingNavigatorKey!.currentState != null &&
+              searchingNavigatorKey!.currentState!.canPop()) {
+            searchingNavigatorKey!.currentState!
+                .popUntil((route) => route.isFirst);
+          }
+          break;
+        case 2:
+          if (settingsNavigatorKey!.currentState != null &&
+              settingsNavigatorKey!.currentState!.canPop()) {
+            settingsNavigatorKey!.currentState!
+                .popUntil((route) => route.isFirst);
+          }
+          break;
+      }
     } else {
       currentIndex.value = index;
     }
+    _initPageBottomBar(index);
   }
 }
